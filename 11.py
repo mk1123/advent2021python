dcb6d51f2d2d93ba441dab8dc2631b3398b993f7
Manan Khattar <mkhattar@berkeley.edu>
2021-12-11 17:13:20 -0800

    day 11 changes

--- 11.py
+++ 11.py
@@ -1,23 +1,75 @@
 from aocd import lines  # type: ignore
 import utils
-from typing import cast, List
+from typing import Dict, Tuple, cast, List
+import collections
+import itertools
 
-doing_part_a = True
-actually_submit = False
-sample = """"""
+doing_part_a = False
+actually_submit = True
+sample = """5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"""
 
 if not actually_submit:
     typed_lines = sample.split("\n")
 else:
     typed_lines = cast(List[str], lines)
 
+num_steps = 100
+flash_count = 0
+curr_flash_count = 0
+grid = utils.parse_grid(typed_lines, "", int)
+flashed: Dict[Tuple[int, int], bool] = collections.defaultdict(bool)
+
+
+def flash_grid() -> int:
+    global curr_flash_count
+    curr_flash_count = 0
+    for coord, val in grid.items():
+        if val > 9:
+            grid[coord] = 1
+        else:
+            grid[coord] += 1
+    for coord in grid:
+        if grid[coord] > 9 and not flashed[coord]:
+            flash(coord)
+    return curr_flash_count
+
+
+def flash(coord: Tuple[int, int]) -> None:
+    global curr_flash_count
+    curr_flash_count += 1
+    flashed[coord] = True
+    for neighbor_coord, _ in utils.get_surrounding(coord, grid, True):
+        grid[neighbor_coord] += 1
+        if grid[neighbor_coord] > 9 and not flashed[neighbor_coord]:
+            flash(neighbor_coord)
+
 
 def a() -> int:
-    return 0
+    for _ in range(num_steps):
+        global flash_count
+        flash_count += flash_grid()
+        global flashed
+        flashed = collections.defaultdict(bool)
+    return flash_count
 
 
 def b() -> int:
-    return 0
+    for step in itertools.count():
+        flash_grid()
+        if curr_flash_count == len(grid):
+            return step + 1
+        global flashed
+        flashed = collections.defaultdict(bool)
+    return -1
 
 
-utils.submit(a(), actually_submit)
+utils.submit(a() if doing_part_a else b(), actually_submit)
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 from aocd import submit as sbmt  # type: ignore
-from typing import List, Any, Tuple
+from typing import Callable, Dict, Generator, Generic, List, Any, Tuple, TypeVar
+import itertools
+
+T = TypeVar("T")
 
 
 def int_map(lst: List[Any]) -> List[int]:
@@ -7,14 +10,18 @@ def int_map(lst: List[Any]) -> List[int]:
 
 
 def get_surrounding(
-    i: int, j: int, grid: List[List[Any]]
-) -> List[Tuple[Tuple[int, int], Any]]:
-    width, height = len(grid), len(grid[0])
-    deltas = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    coord: Tuple[int, int], grid: Dict[Tuple[int, int], T], with_diagonal: bool = False
+) -> List[Tuple[Tuple[int, int], T]]:
+    i, j = coord
+    deltas: List[Tuple[int, int]] = (
+        [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        if not with_diagonal
+        else [(-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1), (0, 1), (0, -1)]
+    )
     res = []
     for dx, dy in deltas:
-        if 0 <= (i + dx) < width and 0 <= (j + dy) < height:
-            res.append(((i + dx, j + dy), grid[i + dx][j + dy]))
+        if (new_coord := (i + dx, j + dy)) in grid:
+            res.append((new_coord, grid[new_coord]))
     return res
 
 
@@ -23,3 +30,33 @@ def submit(output: int, should_submit: bool = False) -> None:
         sbmt(output)
     else:
         print(output)
+
+
+def parse_grid(
+    input: List[str], sep: str, map_func: Callable[[str], T]
+) -> Dict[Tuple[int, int], T]:
+    res: Dict[Tuple[int, int], T] = {}
+    for i, line in enumerate(input):
+        for j, letter in enumerate(line if not sep else line.split(sep)):
+            res[(i, j)] = map_func(letter)
+    return res
+
+
+def dimensions_grid(grid: Dict[Tuple[int, int], T]) -> Tuple[int, int]:
+    rows = 0
+    for i in itertools.count():
+        if (i, 0) not in grid:
+            break
+        rows += 1
+    cols = 0
+    for j in itertools.count():
+        if (0, j) not in grid:
+            break
+        cols += 1
+    return rows, cols
+
+
+def print_grid(grid: Dict[Tuple[int, int], T]) -> None:
+    rows, cols = dimensions_grid(grid)
+    for i in range(rows):
+        print([grid[(i, j)] for j in range(cols)])
